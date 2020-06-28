68ca95a5a7970a266bc4955166b9c6eda7957d1b
Narek Harutyunyan <19narek97@gmail.com>
2020-06-28 15:21:07 +0400

    The initial look of my project

---
+++ src/routers.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Routers extends React.Component{
+
+
+    render() {
+        return (
+            <div>
+                Router
+            </div>
+        )
+    }
+}
+
+export default Routers
