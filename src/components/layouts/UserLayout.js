a9c0ce00670c5171af9fae371099fd764b737c86
Narek Harutyunyan <19narek97@gmail.com>
2020-06-28 16:02:52 +0400

    created routers and home page

---
+++ src/components/layouts/UserLayout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Container,Row} from "react-bootstrap"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {Link} from "react-router-dom"
+
+class UserLayout extends React.Component {
+
+
+    render() {
+        const {children} = this.props;
+
+        return (
+            <Container fluid={true}>
+                <Row style = {{display:'block'}}>
+                    <Navbar bg="light" expand='md'>
+                        <Navbar.Brand href="/">Users</Navbar.Brand>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Link style={{marginTop:'6px'}} to={"/"}>Home</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+
+                </Row>
+                {children}
+            </Container>
+        )
+    }
+
+}
+
+export default UserLayout
--- src/routers.js
+++ src/routers.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {Switch} from 'react-router'
+import AppRouter from './components/AppRouter/AppRouter'
+import UserLayout from "./components/layouts/UserLayout";
+import Home from  "./view/Home"
 
 class Routers extends React.Component{
 
@@ -6,7 +10,9 @@ class Routers extends React.Component{
     render() {
         return (
             <div>
-                Router
+                <Switch>
+                    <AppRouter exact path={'/'} layout={UserLayout} component={Home} />
+                </Switch>
             </div>
         )
     }
